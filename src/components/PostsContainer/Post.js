4540b40dce973c5fc67b40ca0c5345fe2b760fee
JGPico <minq_2@hotmail.com>
2020-02-18 15:00:01 -0700

    Reached MVP

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,11 +9,11 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
-  // const [likes, setLikes] = useState(props.dataProps.likes);
+  const [likes, setLikes] = useState(props.dataProps.likes);
 
-  // const incrementLikes = () => {
-  //   setLikes(likes => likes + 1);
-  // }
+  const incrementLikes = () => {
+    setLikes(likes => likes + 1);
+  }
 
   return (
     <div className="post-border">
@@ -30,7 +30,7 @@ const Post = props => {
           src={props.dataProps.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection incrementLikes={incrementLikes} likes={likes}/>
       <CommentSection
         postId={props.dataProps.imageUrl}
         comments={props.dataProps.comments}
